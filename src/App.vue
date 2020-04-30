db97159b17e2f3ad0982098c3463d3eceed4ebbd
stanus <kucharik.s@gmail.com>
2020-04-30 12:11:15 +0200

    add basic app styles and test quasar element

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-    <div id="app"></div>
+    <div id="app">
+        <q-input v-model="text" label="Standard" />
+    </div>
 </template>
 
 <script lang="ts">
@@ -8,10 +10,23 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
     components: {},
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    text = '';
+}
 </script>
 
 <style lang="scss">
 #app {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
 }
 </style>
