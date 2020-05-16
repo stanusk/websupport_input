5b37c4cfb541489ccd43594b50928232fb12389a
stanus <kucharik.s@gmail.com>
2020-05-16 12:52:45 +0200

    DRY WsCoupon.vue using $attrs & $listeners

--- src/components/WsCoupon.vue
+++ src/components/WsCoupon.vue
@@ -1,36 +1,19 @@
 <template>
     <ws-custom-input
         label="Insert coupon"
-        :dense="dense"
-        :required="true"
-        :value="value"
         :with-button="true"
-        :dark="dark"
         @button-click="addCoupon"
-        @input="onInput"
+        v-on="$listeners"
+        v-bind="$attrs"
     ></ws-custom-input>
 </template>
 
 <script lang="ts">
-import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import { Component, Emit, Vue } from 'vue-property-decorator';
 import WsCustomInput from '@/components/WsCustomInput.vue';
 
 @Component({ components: { WsCustomInput } })
 export default class WsCoupon extends Vue {
-    @Prop({ default: '' })
-    value!: string;
-
-    @Prop({ default: false })
-    dense!: boolean;
-
-    @Prop({ default: false })
-    dark!: boolean;
-
-    @Emit('input')
-    onInput(value: string) {
-        return value;
-    }
-
     @Emit('add-coupon')
     addCoupon(value: string) {
         return value;
